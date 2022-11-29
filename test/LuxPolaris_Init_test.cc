d2d1903dff5466f7a71bbb1284d707b6b1d91609
lutianen <tianenlu957@gmail.com>
2022-11-29 15:28:46 +0800

    Add: LuxLog and googletest init test.

---
+++ test/LuxPolaris_Init_test.cc
@@ -0,0 +1,17 @@
+
+#include "Logger.h"
+#include <gtest/gtest.h>
+
+#include <iostream>
+
+TEST(LuxPolaris, gest) { ASSERT_TRUE(1 == 1); }
+
+
+int
+main(int argc, char* argv[]) {
+    LOG_INFO << "GoogleTest";
+
+    testing::InitGoogleTest(&argc, argv);
+
+    return RUN_ALL_TESTS();
+}
